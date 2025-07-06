346bf16660c839857d6a81f5600650f2f81d595f
Ceravia <ceraviagaming@gmail.com>
2025-07-05 19:51:57 -0500

    Ported over unfinished IR menu

--- src/frontend/qt_sdl/Window.h
+++ src/frontend/qt_sdl/Window.h
@@ -151,6 +151,8 @@ private slots:
     void onMPSettingsFinished(int res);
     void onOpenWifiSettings();
     void onWifiSettingsFinished(int res);
+    void onOpenIRSettings();
+    void onIRSettingsFinished(int res);
     void onOpenFirmwareSettings();
     void onFirmwareSettingsFinished(int res);
     void onOpenPathSettings();
@@ -270,6 +272,7 @@ public:
     QAction* actAudioSettings;
     QAction* actMPSettings;
     QAction* actWifiSettings;
+    QAction* actIRSettings;
     QAction* actFirmwareSettings;
     QAction* actPathSettings;
     QAction* actInterfaceSettings;
@@ -298,3 +301,4 @@ public:
 };
 
 #endif // WINDOW_H
+

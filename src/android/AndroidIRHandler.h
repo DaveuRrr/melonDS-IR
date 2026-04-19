853505f7cc2aea21e7db47484a9b2b722ef64c8f
Dave uRrr <janosandron@gmail.com>
2026-04-18 23:56:07 -0700

    Rebase Cleanup again

--- src/android/AndroidIRHandler.h
+++ src/android/AndroidIRHandler.h
@@ -9,7 +9,13 @@ namespace MelonDSAndroid
         virtual void closeSerial() = 0;
         virtual int writeSerial(const char* data, int length) = 0;
         virtual int readSerial(char* buffer, int maxLength) = 0;
+        virtual int readSerialBlocking(char* buffer, int maxLength, long long timeoutMs) = 0;
         virtual bool isSerialOpen() = 0;
+        virtual bool openTCP() = 0;
+        virtual void closeTCP() = 0;
+        virtual int writeTCP(const char* data, int length) = 0;
+        virtual int readTCP(char* buffer, int maxLength) = 0;
+        virtual bool isTCPOpen() = 0;
         virtual bool hasDataAvailable() = 0;
         virtual ~AndroidIRHandler() {};
     };
